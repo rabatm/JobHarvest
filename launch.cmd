7f41d993aed91ee500d428dad327e13dc3a3ba95
RABAT Martin <martin.Rabat@gmail.Com>
2024-06-25 18:12:45 +0200

    mod sh

--- launch.cmd
+++ launch.cmd
@@ -1,6 +1,9 @@
 @echo off
 echo Installation des dépendances...
+taskkill /IM chrome.exe /F
 cd %~dp0
+echo Installation des dépendances...
 pip install -r requirements.txt
 echo Exécution du script Python...
 python main.py
+taskkill /IM chrome.exe /F
